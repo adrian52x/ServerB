3c6f6d9737e6cbb32e0c19dcf075d9dc27d08cf7
krishnaamen <krishnaament@gmail.com>
2021-12-16 15:25:09 +0100

    final touch up

--- src/main/java/com/example/serverb/controllers/MainController.java
+++ src/main/java/com/example/serverb/controllers/MainController.java
@@ -2,17 +2,13 @@ package com.example.serverb.controllers;
 
 import com.example.serverb.entities.FriendList;
 import com.example.serverb.entities.Request;
-import com.example.serverb.repos.FriendListRepository;
-import com.example.serverb.repos.UserRepository;
 import com.example.serverb.services.FriendListService;
 import com.example.serverb.services.RequestService;
 import com.example.serverb.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,9 +16,7 @@ import java.util.List;
 @Controller
 public class MainController {
     List<Request> requestList = new ArrayList<>();
-    int  currentId =0;
-
-
+    int currentId = 0;
 
 
     UserService userService;
@@ -36,38 +30,41 @@ public class MainController {
     }
 
     @GetMapping("/acceptRequest/{currentId1}/{requestId}")
-    public String acceptRequest(@PathVariable int currentId1, @PathVariable int requestId){
+    public String acceptRequest(@PathVariable int currentId1, @PathVariable int requestId) {
         Request request = requestService.findRequestById(requestId).get();
-        friendListService.saveInFriendList(new FriendList(request.getUser(),request.getUserEmail(),request.getForeignUserId(),request.getForeignUserEmail(),request.getSenderIp()));
+        friendListService.saveInFriendList(new FriendList(request.getUser(), request.getUserEmail(), request.getForeignUserId(), request.getForeignUserEmail(), request.getSenderIp()));
         requestService.deleteRequest(request);
         requestList = requestService.findRequestsByUserId(currentId1);
-        currentId= currentId1;
-
+        currentId = currentId1;
 
 
         return "redirect:/";
     }
 
     @GetMapping("/declineRequest/{requestId}")
-    public String declineRequest( @PathVariable int requestId){
+    public String declineRequest(@PathVariable int requestId) {
         Request request = requestService.findRequestById(requestId).get();
         requestService.deleteRequest(request);
-        currentId= request.getUser().getId();
+        currentId = request.getUser().getId();
         requestList = requestService.findRequestsByUserId(currentId);
 
         return "redirect:/";
     }
+
     @GetMapping("/")
-    public String getIndex(Model model){
-        model.addAttribute("myRequestList",requestList);
-        model.addAttribute("currentId1",currentId);
+    public String getIndex(Model model) {
+        requestList = requestService.findRequestsByUserId(currentId);
+
+        model.addAttribute("myRequestList", requestList);
+        model.addAttribute("currentId1", currentId);
 
         return "index";
     }
+
     @GetMapping("/friendList/{currentId1}")
-    public String getFriendList(@PathVariable int currentId1, Model model){
+    public String getFriendList(@PathVariable int currentId1, Model model) {
         List<FriendList> friendList = friendListService.findFriendListByUserId(currentId1);
-        model.addAttribute("friendList",friendList);
+        model.addAttribute("friendList", friendList);
         return "friendList";
 
     }
